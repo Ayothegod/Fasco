22dd06a099ac25baf70c190e460e71e06925b6e7
Ayothegod <heyayomideadebisi@gmail.com>
2024-08-02 18:17:47 +0100

    complete cart, and individual page ui

--- app/routes/shop.tsx
+++ app/routes/shop.tsx
@@ -24,6 +24,7 @@ import { Input } from "../components/ui/input";
 import {
   Form,
   json,
+  Link,
   redirect,
   useLoaderData,
   useSearchParams,
@@ -39,7 +40,9 @@ import {
 import Newsletter from "~/components/build/Newsletter";
 import FollowUs from "~/components/build/FollowUs";
 import clsx from "clsx";
-import useSWR from 'swr'
+import useSWR from "swr";
+import { Skeleton } from "~/components/ui/skeleton";
+import slider from "~/assets/fascoAsset/slider.png";
 // export async function Loader({ request }: LoaderFunctionArgs) {
 //   try {
 //     const url = new URL(request.url);
@@ -155,14 +158,23 @@ import useSWR from 'swr'
 //   console.log("NOTAVALUE");
 //   return null;
 
-const fetcher = (url: any) => fetch(url).then(res => res.json())
+const fetcher = (url: any) => fetch(url).then((res) => res.json());
 
 export default function Shop() {
-  const { data, error, isLoading } = useSWR('https://dummyjson.com/products/category/womens-dresses', fetcher)
-  // console.log(data.products);
-  
-  const [defaultLayout, setDefaultLayout] = useState("grid");
+  const { data, error, isLoading } = useSWR(
+    "https://dummyjson.com/products/category/womens-dresses",
+    fetcher
+  );
+  // console.log(data);
+  const [products, setProducts] = useState([]);
+  useEffect(() => {
+    if (data) {
+      setProducts(data.products); // Assuming data has a `products` field
+    }
+  }, [data]);
+  // console.log(products);
 
+  const [defaultLayout, setDefaultLayout] = useState("grid");
   const updateDefaultLayout = () => {
     if (defaultLayout === "grid") {
       setDefaultLayout("flat");
@@ -205,8 +217,6 @@ export default function Shop() {
             </BreadcrumbItem>
           </BreadcrumbList>
         </Breadcrumb>
-
-        {/* <Button onClick={handleSearch}>Add search</Button> */}
       </section>
 
       <section className="pageStyle mt-8 flex gap-x-4">
@@ -316,22 +326,55 @@ export default function Shop() {
             </div>
           </div>
 
-          <div>
-            {isLoading? 
-            <div>
-              
-            </div>
-            : "data"}
+          <div className="mt-4">
+            {isLoading ? (
+              <div>
+                <Skeleton className="h-[125px] w-[250px] rounded-xl" />
+                <div className="space-y-2">
+                  <Skeleton className="h-4 w-[250px]" />
+                  <Skeleton className="h-4 w-[200px]" />
+                </div>
+              </div>
+            ) : (
+              <div>
+                {defaultLayout === "grid" ? (
+                  <div className="grid grid-cols-1 sm:grid-cols-2 md:grid-cols-3 gap-4">
+                    {products.map((product: any, index) => (
+                      <Link
+                        to={`/products/${product.id}`}
+                        key={product.id}
+                        className=""
+                      >
+                        <img
+                          src={product.thumbnail || "https://placehold.co/400"}
+                          alt={`image-${product.title}`}
+                          className=" w-full h-auto"
+                        />
+                        <div className="mt-4">
+                          <p className="font-medium">{product.title}</p>
+                          <div className="flex items-center justify-between text-sm">
+                            <p>${product.price}</p>
+                            <p>{product.availabilityStatus}</p>
+                          </div>
+                        </div>
+                      </Link>
+                    ))}
+                  </div>
+                ) : (
+                  "Hello"
+                )}
+              </div>
+            )}
           </div>
         </main>
       </section>
 
-      {/* <section className="hidden md:block mt-16 mb-16"> */}
-      {/* <img src={slider} alt="top-banner-image" className="" /> */}
-      {/* </section> */}
+      <section className="hidden md:block mt-16 mb-16">
+      <img src={slider} alt="top-banner-image" className="" />
+      </section>
 
-      {/* <FollowUs /> */}
-      {/* <Newsletter /> */}
+      <FollowUs />
+      <Newsletter />
     </main>
   );
 }

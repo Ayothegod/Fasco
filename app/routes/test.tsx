913257962f3a1aee5364c7d2d277817129a55403
Ayothegod <heyayomideadebisi@gmail.com>
2024-08-22 05:33:50 +0100

    init

---
+++ app/routes/test.tsx
@@ -0,0 +1,12 @@
+import React from "react";
+
+export default function Test() {
+  return (
+    <div>
+      <input type="text" placeholder="Enter cost" />
+      <input type="text" placeholder="Enter quantity" />
+      <input type="text" placeholder="Enter value" />
+      <button>Submit</button>
+    </div>
+  );
+}

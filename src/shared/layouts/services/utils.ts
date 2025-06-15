76496de4953f062d5872b36a935ebeec980ad1da
Ayomide <heyayomideadebisi@gmail.com>
2025-06-15 11:14:56 +0100

    fix: refactoring old remix code

---
+++ src/shared/layouts/services/utils.ts
@@ -0,0 +1,13 @@
+export const noUserNavLink = [
+  { name: "Home", href: "/" },
+  { name: "Shop", href: "/shop" },
+  { name: "New Arrivals", href: "#new-arrivals" },
+  { name: "Sign in", href: "/login" },
+];
+
+export const navLink = [
+  { name: "Home", href: "/" },
+  { name: "Shop", href: "/shop" },
+  { name: "Products", href: "/products" },
+  { name: "pages", href: "#" },
+];

bd2c3382dd10b0025de9c4f4e44f736b449931b6
Ayomide <heyayomideadebisi@gmail.com>
2025-06-20 10:54:08 +0100

    feat: initialize client and server adapter for sanity

--- src/core/config/sanityClient.ts
+++ src/core/config/sanityClient.ts
@@ -1,16 +1,16 @@
-// import { createClient } from "@sanity/client";
+import { createClient } from "@sanity/client";
 
-// export const client = createClient({
-//   projectId: import.meta.env.SANITY_PROJECT_ID,
-//   dataset: "production",
-//   apiVersion: "2024-06-01",
-//   useCdn: false,
-// });
+export const client = createClient({
+  projectId: import.meta.env.SANITY_PROJECT_ID,
+  dataset: "production",
+  apiVersion: "2024-06-01",
+  useCdn: false,
+});
 
-// export const server = createClient({
-//   projectId: import.meta.env.SANITY_PROJECT_ID,
-//   dataset: "production",
-//   apiVersion: "2024-06-01",
-//   token: import.meta.env.SANITY_TOKEN,
-//   useCdn: false,
-// });
+export const server = createClient({
+  projectId: import.meta.env.SANITY_PROJECT_ID,
+  dataset: "production",
+  apiVersion: "2024-06-01",
+  token: import.meta.env.SANITY_TOKEN,
+  useCdn: false,
+});
